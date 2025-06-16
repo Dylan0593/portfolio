c01f227b9bb425ec5287b19e9ad450bd7fdb3ccf
dylan akpan <dakpan0593@gmail.com>
2025-06-16 04:39:22 +0100

    portfolio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def powershare
+  end
+
+
+  def twenty_one_days_later
+  end
+
+
+  def about_me
+  end
+
+
+  def cv
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  controller :pages do
+    root to: "pages#home"
+    get :powershare
+    get :twenty_one_days_later
+    get :about_me
+    get :cv
+    # can put more later???? dylan
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
