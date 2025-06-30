6e267b26793cb3bf87170c3956d42ef0a534b1dc
dylan akpan <dakpan0593@gmail.com>
2025-06-30 10:51:22 +0100

    Add SendGrid SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,19 @@ Rails.application.configure do
   protocol: 'https'
   }
 
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 587,
+  domain: "herokuapp.com",
+  user_name: ENV["SENDGRID_USERNAME"], # better with ENV vars
+  password: ENV["SENDGRID_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+  }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
